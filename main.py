c386e5953541460a9d6fad66c1211fe4d6cb4b52
Ernesto Casablanca <casablancaernesto@gmail.com>
2023-10-17 22:23:26 +0100

    feat: avoid initializing variable outside of main

--- main.py
+++ main.py
@@ -6,11 +6,8 @@ from discord.ext import commands
 
 from config import Config
 
-intents = discord.Intents.all()
-client = commands.Bot(command_prefix="!", intents=intents)
 
-
-async def load():
+async def load(client: "commands.Bot"):
     for filename in os.listdir("./cogs"):
         if filename.endswith(".py"):
             await client.load_extension(f"cogs.{filename[:-3]}")
@@ -18,8 +15,11 @@ async def load():
 
 # Loads all cogs then starts the bot
 async def main():
-    await load()
+    intents = discord.Intents.all()
+    client = commands.Bot(command_prefix="!", intents=intents)
+    await load(client)
     await client.start(Config.get("TOKEN"))
 
 
-asyncio.run(main())
+if __name__ == "__main__":
+    asyncio.run(main())
